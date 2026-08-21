8482700fcaf29a1081c182246d4147d3df27d5ae
Luiz Miguel Bastos <luizmiguelbastos90@gmail.com>
2026-08-21 13:48:41 -0300

    feat: criacao da interface UserRepository com JPA

--- src/main/java/com/luizMiguel/runna/user/UserRepository.java
+++ src/main/java/com/luizMiguel/runna/user/UserRepository.java
@@ -1,4 +1,10 @@
 package com.luizMiguel.runna.user;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<UserModel, UUID> {
+    Optional<UserModel> findByUsername(String username);
 }
