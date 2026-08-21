da57ac705abbba3d1a7d0e0b55b3b71911e7f6f9
Luiz Miguel Bastos <luizmiguelbastos90@gmail.com>
2026-08-21 11:17:13 -0300

    Inicializacao

---
+++ src/main/java/com/luizMiguel/runna/RunnaApplication.java
@@ -0,0 +1,13 @@
+package com.luizMiguel.runna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunnaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunnaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luizMiguel/runna/RunnaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luizMiguel.runna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunnaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
