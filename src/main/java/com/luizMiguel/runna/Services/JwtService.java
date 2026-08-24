f1779d7801a5dc37af51b238394099fe291119ed
Luiz Miguel Bastos <luizmiguelbastos90@gmail.com>
2026-08-24 19:07:40 -0300

    feat: Configuracao do SpringSecurity e implementacao do JWT Service

---
+++ src/main/java/com/luizMiguel/runna/Services/JwtService.java
@@ -0,0 +1,43 @@
+package com.luizMiguel.runna.Services;
+
+import com.luizMiguel.runna.Models.UserModel;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+
+@Service
+public class JwtService {
+
+    private final SecretKey secretKey;
+    private final long expiration;
+
+    public JwtService(
+            @Value("${jwt.secret}") String secret,
+            @Value("${jwt.expiration}") long expiration
+    ) {
+        this.secretKey = Keys.hmacShaKeyFor(
+                secret.getBytes(StandardCharsets.UTF_8)
+        );
+
+        this.expiration = expiration;
+    }
+
+    public String gerarToken(UserModel user) {
+
+        Date agora = new Date();
+        Date expiracao = new Date(agora.getTime() + expiration);
+
+        return Jwts.builder()
+                .subject(user.getId().toString())
+                .issuedAt(agora)
+                .expiration(expiracao)
+                .signWith(secretKey)
+                .compact();
+    }
+}
--- src/main/java/com/luizMiguel/runna/Services/UserService.java
+++ src/main/java/com/luizMiguel/runna/Services/UserService.java
@@ -1,18 +1,41 @@
 package com.luizMiguel.runna.Services;
 
+import com.luizMiguel.runna.Models.UserModel;
 import com.luizMiguel.runna.Repositorys.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
     private final UserRepository userRepository;
-    public UserService(UserRepository userRepository) {
+    private final PasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
+ public UserModel createUser (String username, String password){
+     UserModel user = new UserModel();
 
+     user.setUsername(username);
+     user.setPassword(passwordEncoder.encode(password));
 
+     return userRepository.save(user);
+ }
+
+    public UserModel userLogin(String username, String password) {
+
+        UserModel user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new RuntimeException("Credenciais inválidas"));
+
+        if (!passwordEncoder.matches(password, user.getPassword())) {
+            throw new RuntimeException("Credenciais inválidas");
+        }
+
+        return user;
+    }
 
 
 
