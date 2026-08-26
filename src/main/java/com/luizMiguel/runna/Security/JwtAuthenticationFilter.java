02f7106497e7c8ba83c4cfba1f36e3243b867dfa
Luiz Miguel Bastos <luizmiguelbastos90@gmail.com>
2026-08-26 19:46:22 -0300

    feat: implementacao da validacao de token no JwtAuthenticationFilter

--- src/main/java/com/luizMiguel/runna/Security/JwtAuthenticationFilter.java
+++ src/main/java/com/luizMiguel/runna/Security/JwtAuthenticationFilter.java
@@ -1,12 +1,17 @@
 package com.luizMiguel.runna.Security;
 
+import io.jsonwebtoken.JwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.UUID;
 
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
@@ -31,6 +36,14 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             return;
         }
         token = authHeader.substring(7);
+
+        try {
+           UUID userId = jwtService.extractUserId(token);
+            var auth = new UsernamePasswordAuthenticationToken(userId, null, List.of());
+            SecurityContextHolder.getContext().setAuthentication(auth);
+        } catch (JwtException | IllegalArgumentException e){
+            SecurityContextHolder.clearContext();
+        }
 
 
         filterChain.doFilter(request, response);
