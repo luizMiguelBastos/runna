a2cdbe8a4b2bded9226c03a31c805015ccd3d5e4
Luiz Miguel Bastos <luizmiguelbastos90@gmail.com>
2026-08-28 19:46:29 -0300

    feat: Configuracao dos metodos presentes no UserController

--- src/main/java/com/luizMiguel/runna/Controllers/UserController.java
+++ src/main/java/com/luizMiguel/runna/Controllers/UserController.java
@@ -1,7 +1,29 @@
 package com.luizMiguel.runna.Controllers;
 
+import com.luizMiguel.runna.DTOs.User.CreateUserRequest;
+import com.luizMiguel.runna.DTOs.User.UserResponse;
+import com.luizMiguel.runna.Models.UserModel;
+import com.luizMiguel.runna.Services.UserService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RestController
+@RequestMapping("/users")
 public class UserController {
+
+    private final UserService userService;
+
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping()
+    public UserResponse create (@RequestBody CreateUserRequest user){
+        return userService.createUser(user.username(), user.password());
+    }
 }
