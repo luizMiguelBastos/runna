212ca741acd8f9ac49e87e86672217153b3596a7
Luiz Miguel Bastos <luizmiguelbastos90@gmail.com>
2026-08-28 14:59:30 -0300

    feat: mudanca nos atributos dos DTOs CreateUserRequest

--- src/main/java/com/luizMiguel/runna/DTOs/User/CreateUserRequest.java
+++ src/main/java/com/luizMiguel/runna/DTOs/User/CreateUserRequest.java
@@ -1,6 +1,5 @@
 package com.luizMiguel.runna.DTOs.User;
 
-import java.util.UUID;
 
-public record CreateUserRequest(UUID id, String username) {
+public record CreateUserRequest(String username, String password) {
 }
